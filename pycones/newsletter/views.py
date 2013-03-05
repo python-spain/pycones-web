c1654a7d67348dbe48bb9c5086b9dfba79a586c8
Andrey Antukh <niwi@niwi.be>
2013-03-05 23:09:15 +0100

    Refactor welcome email-send function.

--- pycones/newsletter/views.py
+++ pycones/newsletter/views.py
@@ -4,6 +4,7 @@ import uuid
 
 from django.shortcuts import render_to_response,redirect
 from django.contrib.auth.models import User
+from django.template.loader import render_to_string
 from django.template import RequestContext
 from django import http
 
@@ -14,32 +15,11 @@ def send_welcome_msg(email_user,token):
     subject = u'¡Bienvenido a PyConES!'
     from_email = u'newsletter@es.pycon.org'
 
-    # FIXME: put this text into one template.
-    text_content = u'''¡Bienvenido a la newsletter de PyConES!
-
-Hola,
-
-Muchas gracias por registrarte a la newsletter de la Python Conference de
-A partir de ahora recibirás todas las noticias y actualizaciones sobre el
-eventp directamente en tu email sin esfuerzo alguno. Serás el primero en
-recibir información crítica del evento como, por ejemplo, noticias sobre
-ponentes y patrocinadores, venta de entradas, planificación de charlas..
-
-Recuerda que también estamos en twitter en @PyConES y #PyConES. Estamos
-encantados de escucharte así que no dudes en mandarnos un tweet :)
-
-Aún así no te obligamos a aguantarnos. Podrás darte de baja con el siguiente
-enlace:
-
-http://es.pycon.org/newsletter/unsuscribe/?email={0}&val_token={1}
-
-Un saludo
-Muchas gracias
-Equipo Organizativo de PyConES
-    '''.format(email_user,token)
+    context = {"email": email_user, "token": token}
     context_email = {'email_user' : email_user, 'token' : token}
-    template_email = get_template('newsletter_welcome_mail.html')
-    html_content = template_email.render(Context(context_email))
+
+    text_content = render_to_string("newsletter_welcome_mail.txt", context)
+    html_content = render_to_string("newsletter_welcome_mail.html", context)
 
     msg = EmailMultiAlternatives(subject, text_content, from_email, [email_user])
     msg.attach_alternative(html_content, "text/html")
