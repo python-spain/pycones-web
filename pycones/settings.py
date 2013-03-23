9fbde493128877ed749b38c1be4f1fbce75f1d34
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2013-03-23 11:53:31 +0100

    Ensure boolean USE_TLS

--- pycones/settings.py
+++ pycones/settings.py
@@ -217,7 +217,7 @@ if not DEBUG:
     EMAIL_PORT = os.getenv('EMAIL_PORT')
     EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-    EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', False)
+    EMAIL_USE_TLS = bool(os.getenv('EMAIL_USE_TLS', False))
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
